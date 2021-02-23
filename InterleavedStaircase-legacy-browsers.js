35bf4973a07f6c3c1f65231f566feb10c153d0bf
David Petrescu <david.petrescu@manchester.ac.uk>
2021-02-23 16:26:16 +0000

    _

--- InterleavedStaircase-legacy-browsers.js
+++ InterleavedStaircase-legacy-browsers.js
@@ -58,8 +58,6 @@ psychoJS.start({
 
 psychoJS.experimentLogger.setLevel(core.Logger.ServerLevel.EXP);
 
-
-var frameDur;
 function updateInfo() {
   expInfo['date'] = util.MonotonicClock.getDateStr();  // add a simple timestamp
   expInfo['expName'] = expName;
@@ -79,21 +77,6 @@ function updateInfo() {
   return Scheduler.Event.NEXT;
 }
 
-
-var InstructionsClock;
-var text;
-var key_resp;
-var set_conditionClock;
-var text_3;
-var trialClock;
-var leftImagePortrait;
-var rightImagePortrait;
-var text_2;
-var resp;
-var endClock;
-var text_4;
-var globalClock;
-var routineTimer;
 function experimentInit() {
   // Initialize components for Routine "Instructions"
   InstructionsClock = new util.Clock();
@@ -176,12 +159,6 @@ function experimentInit() {
   return Scheduler.Event.NEXT;
 }
 
-
-var t;
-var frameN;
-var continueRoutine;
-var _key_resp_allKeys;
-var InstructionsComponents;
 function InstructionsRoutineBegin(snapshot) {
   return function () {
     //------Prepare to start Routine 'Instructions'-------
@@ -205,7 +182,6 @@ function InstructionsRoutineBegin(snapshot) {
     return Scheduler.Event.NEXT;
   }
 }
-
 
 function InstructionsRoutineEachFrame(snapshot) {
   return function () {
@@ -274,7 +250,6 @@ function InstructionsRoutineEachFrame(snapshot) {
   };
 }
 
-
 function InstructionsRoutineEnd(snapshot) {
   return function () {
     //------Ending Routine 'Instructions'-------
@@ -290,9 +265,6 @@ function InstructionsRoutineEnd(snapshot) {
   };
 }
 
-
-var trials_2;
-var currentLoop;
 function trials_2LoopBegin(trials_2LoopScheduler) {
   // set up handler to look after randomisation of conditions etc
   trials_2 = new TrialHandler({
@@ -323,8 +295,6 @@ function trials_2LoopBegin(trials_2LoopScheduler) {
   return Scheduler.Event.NEXT;
 }
 
-
-var trials;
 function trialsLoopBegin(trialsLoopScheduler) {
   // set up handler to look after randomisation of conditions etc
   trials = new TrialHandler({
@@ -351,13 +321,11 @@ function trialsLoopBegin(trialsLoopScheduler) {
   return Scheduler.Event.NEXT;
 }
 
-
 function trialsLoopEnd() {
   psychoJS.experiment.removeLoop(trials);
 
   return Scheduler.Event.NEXT;
 }
-
 
 function trials_2LoopEnd() {
   psychoJS.experiment.removeLoop(trials_2);
@@ -365,30 +333,6 @@ function trials_2LoopEnd() {
   return Scheduler.Event.NEXT;
 }
 
-
-var imageSet;
-var imageSetStr;
-var setName;
-var nRefImage;
-var initStep;
-var noOfMiniScenes;
-var reversals;
-var trialCounter;
-var reversalsSharp;
-var reversalsFlat;
-var levList;
-var respList;
-var kestList;
-var reverseList;
-var localTrialNo;
-var globalTrialNo;
-var maxNoFlat;
-var images1;
-var images2;
-var images3;
-var leftImage;
-var rightImage;
-var set_conditionComponents;
 function set_conditionRoutineBegin(snapshot) {
   return function () {
     //------Prepare to start Routine 'set_condition'-------
@@ -452,12 +396,9 @@ function set_conditionRoutineBegin(snapshot) {
     images3 = new Array();
     
     function preload(images, set, index) {
-        index = index || 0
-        if (images && 22 > index) {
-                images[i] = new Image ();
-                images[i].onload = function() {
-                    preload(images, set, index + 1);
-                }
+        for(i = 0; i <= 22; i++
+        {
+            image[i] = new Image();
             images[i].src = `stimuli/${imageSetStr}/${set}/${i}_${setName}.jpg`;
         }
     }
@@ -482,8 +423,6 @@ function set_conditionRoutineBegin(snapshot) {
   }
 }
 
-
-var frameRemains;
 function set_conditionRoutineEachFrame(snapshot) {
   return function () {
     //------Loop for each frame of Routine 'set_condition'-------
@@ -531,7 +470,6 @@ function set_conditionRoutineEachFrame(snapshot) {
   };
 }
 
-
 function set_conditionRoutineEnd(snapshot) {
   return function () {
     //------Ending Routine 'set_condition'-------
@@ -544,21 +482,6 @@ function set_conditionRoutineEnd(snapshot) {
   };
 }
 
-
-var staircase;
-var currentScene;
-var currentStaircase;
-var phi;
-var initN;
-var images;
-var trial;
-var refSide;
-var compSide;
-var stairFactor;
-var nCompQuality;
-var comp_num;
-var _resp_allKeys;
-var trialComponents;
 function trialRoutineBegin(snapshot) {
   return function () {
     //------Prepare to start Routine 'trial'-------
@@ -701,7 +624,6 @@ function trialRoutineBegin(snapshot) {
   }
 }
 
-
 function trialRoutineEachFrame(snapshot) {
   return function () {
     //------Loop for each frame of Routine 'trial'-------
@@ -797,8 +719,6 @@ function trialRoutineEachFrame(snapshot) {
   };
 }
 
-
-var kestResp;
 function trialRoutineEnd(snapshot) {
   return function () {
     //------Ending Routine 'trial'-------
@@ -876,8 +796,6 @@ function trialRoutineEnd(snapshot) {
   };
 }
 
-
-var endComponents;
 function endRoutineBegin(snapshot) {
   return function () {
     //------Prepare to start Routine 'end'-------
@@ -898,7 +816,6 @@ function endRoutineBegin(snapshot) {
     return Scheduler.Event.NEXT;
   }
 }
-
 
 function endRoutineEachFrame(snapshot) {
   return function () {
@@ -947,7 +864,6 @@ function endRoutineEachFrame(snapshot) {
   };
 }
 
-
 function endRoutineEnd(snapshot) {
   return function () {
     //------Ending Routine 'end'-------
@@ -959,7 +875,6 @@ function endRoutineEnd(snapshot) {
     return Scheduler.Event.NEXT;
   };
 }
-
 
 function endLoopIteration(scheduler, snapshot) {
   // ------Prepare for next entry------
@@ -983,14 +898,12 @@ function endLoopIteration(scheduler, snapshot) {
   };
 }
 
-
 function importConditions(currentLoop) {
   return function () {
     psychoJS.importAttributes(currentLoop.getCurrentTrial());
     return Scheduler.Event.NEXT;
     };
 }
-
 
 function quitPsychoJS(message, isCompleted) {
   // Check for and save orphaned data
